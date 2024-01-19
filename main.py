ac8fbc2ee39cae8f7ce7d4a80b145e88c6d275e2
Emirh <emirhan.akin@student.hu.nl>
2024-01-19 19:47:50 +0100

    Module 1

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import csv
+import csv  # Heb dit geimporteerd om een csv bestand te openen
 import random
 from datetime import datetime
 
-stations = ['Utrecht', 'Arnhem', 'Almere', 'Amersfoort', 'Breda', 'Amsterdam', 'Den Haag', 'Gouda', 'Rotterdam', 'Hilversum']
+stations = ['Utrecht', 'Arnhem', 'Rotterdam']
 
-naam = input("Voer uw naam in (laat leeg voor anoniem): ")
-bericht = input("Voer uw bericht in (maximaal 140 karakters): ")
+naam = input("Voer uw naam in (laat leeg voor anoniem): ") # Vraag de gebruiker om hun naam in te voeren
+bericht = input("Voer uw bericht in (maximaal 140 karakters): ") # Vraag de gebruiker om hun feedback in te voeren
 
-if len(bericht) > 140:
+if len(bericht) > 140: # Controleer of het bericht niet langer is dan 140 karakters
     print("Het bericht mag maximaal 140 karakters bevatten.")
 else:
-    station = random.choice(stations)
-    datum_tijd = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    if not naam:
+    station = random.choice(stations) # Een random station uit de lijst word gekozen
+    datum_tijd = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # de huidige datum en tijd op van de bericht
+    if not naam: # Als naam niet word doorgegeven dan word de gebruiker zijn naam anoniem
         naam = "anoniem"
 
-    with open("feedback.csv", mode='a', newline='') as file:
+    with open("feedback.csv", mode='a', newline='') as file: # Het CSV-bestand word geopend als 'a' dat staat voor toevoegen
         writer = csv.writer(file)
-        writer.writerow([bericht, datum_tijd, naam, station, "Nog niet gemodereerd"])
+        writer.writerow([bericht, datum_tijd, naam, station, "Nog niet gemodereerd"]) # De gegevens word gestuurd naar het CSV-bestand
 
 
 
